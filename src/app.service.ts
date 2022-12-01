41d7a717164b6e52ee5ccb065fff3d02cfbbadd3
VibekSM <vibek@snappymob.com>
2022-12-01 08:24:03 +0530

    done

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
+import * as soap from 'soap';
 import { Injectable } from '@nestjs/common';
-
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async trustgate(): Promise<any> {
+    const data = await soapPromise;
+    return data;
   }
 }
+
+const soapPromise = new Promise((resolve, reject) => {
+  const wsdlOptions = { useEmptyTag: true };
+  soap.createClient(
+    'http://localhost:8080/MTSAPilot/MyTrustSignerAgentWSEL?WSDL',
+    wsdlOptions,
+    (err: any, client: any) => {
+      if (err) reject(err);
+      client.MyTrustSignerAgentWSEL.MyTrustSignerAgentWSELPort.RequestSMSOTP(
+        { UserID: '770908012232', MobileNo: '7002659475', OTPUsage: 'DS' },
+        function (err: any, result: any) {
+          if (err) reject(err);
+          if (result) resolve(result);
+        },
+      );
+    },
+  );
+});
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -22,3 +22,59 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 });
+
+// const wsdlOptions = { useEmptyTag: true };
+// let resp: any;
+// soap.createClient(
+//   'http://localhost:8080/MTSAPilot/MyTrustSignerAgentWSEL?WSDL',
+//   wsdlOptions,
+//   (err: any, client: any) => {
+//     if (err) console.log(err);
+//     client.MyTrustSignerAgentWSEL.MyTrustSignerAgentWSELPort.RequestSMSOTP(
+//       { UserID: '770908012232', MobileNo: '7002659475', OTPUsage: 'DS' },
+//       function (err: any, result: any) {
+//         if (err) console.log(err);
+//         resp = result;
+//         console.log(result);
+//       },
+//     );
+//   },
+// );
+// return resp;
+
+// const client = soap
+//   .createClientAsync(
+//     'http://localhost:8080/MTSAPilot/MyTrustSignerAgentWSEL?WSDL',
+//     { overridePromiseSuffix: 'Promise' },
+//   )
+//   .then((client) => {
+//     const data =
+//       client.MyTrustSignerAgentWSEL.MyTrustSignerAgentWSELPort.RequestSMSOTP(
+//         {
+//           UserID: '770908012232',
+//           MobileNo: '7002659475',
+//           OTPUsage: 'DS',
+//         },
+//       ).then((res) => console.log(res));
+//   });
+
+// var clientOptions = {};
+// soap.createClient(
+//   'http://localhost:8080/MTSAPilot/MyTrustSignerAgentWSEL?WSDL',
+//   clientOptions,
+//   async function (err, client) {
+//     try {
+//       const data =
+//         await client.MyTrustSignerAgentWSEL.MyTrustSignerAgentWSELPort.RequestSMSOTP(
+//           {
+//             UserID: '770908012232',
+//             MobileNo: '7002659475',
+//             OTPUsage: 'DS',
+//           },
+//         );
+//       console.log(data);
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   },
+// );
